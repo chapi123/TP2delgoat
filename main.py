e47da9a86b09f3985f93052aa2339550c2558493
Chapi123} <schaparro@alumno.huergo.edu.ar>
2026-06-24 17:14:10 -0300

    futurista

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PC_medic import create_medic, create_PC
 from pokemons import load_medals, load_pokemons
+import menus
 
 class Game:
     def __init__(self):
@@ -8,24 +9,15 @@ class Game:
         self.pokedex = load_pokemons()
         self.medals = load_medals()
 
-    def print_menu(self):
-        print("\n" + "=" * 55)
-        print("                    HUERGO POKEMON")
-        print("=" * 55)
-        print(" 1. Ver Pokédex")
-        print(" 2. Ver Equipo Principal")
-        print(" 3. Ver PC")
-        print(" 4. Capturar nuevo Pokémon")
-        print(" 5. Ordenar PC")
-        print(" 6. Buscar Pokémon en Equipo")
-        print(" 7. Enviar Pokémon al Centro Pokémon")
-        print(" 8. Transferir Pokémon al Profesor Oak")
-        print(" 9. Deshacer última transferencia")
-        print("10. Desafiar Líder de Gimnasio")
-        print("11. Salir del sistema")
-        print("=" * 55)
+    def run(self):
+        while True:
+            opcion = menus.main_menu()
+            print(opcion,type(opcion))
 
-        opcion = input("Seleccione una opción: ")
-        return opcion
+            if opcion == 1:
+                opcion1 = menus.pokedex_select()
+                if opcion1 == 1:
+                    for i in self.pokedex:
+                        print(self.pokedex[i].id, self.pokedex[i].name, self.pokedex[i].type, self.pokedex[i].PC)
 
-Game().print_menu()
+Game().run()
---
+++ menus.py
@@ -0,0 +1,27 @@
+def main_menu():
+    print("\n" + "=" * 55)
+    print("                    HUERGO POKEMON")
+    print("=" * 55)
+    print(" 1. View Pokédex")
+    print(" 2. View Main Team")
+    print(" 3. View PC")
+    print(" 4. Capture new Pokémon")
+    print(" 5. Sort PC")
+    print(" 6. Search Pokémon in Team")
+    print(" 7. Send Pokémon to Pokémon Center")
+    print(" 8. Transfer Pokémon to Professor Oak")
+    print(" 9. Undo last transfer")
+    print("10. Challenge Gym Leader")
+    print("11. Exit system")
+    print("=" * 55)
+
+    opcion = int(input("Select an option: "))
+    return opcion
+
+def pokedex_select():
+    print("Select One of the following options: ")
+    print("1. View full Pokedex")
+    print("2. Search a Pokemon by his ID")
+    print("3. Exit")
+    opcion = int(input("Select an option: "))
+    return opcion
